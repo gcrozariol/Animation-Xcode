dfdab517cfa546435f9f32ce91fad3d9b55265cc
Guilherme Crozariol <guilherme@guilhermes-macbook-pro.local>
2016-12-19 14:56:01 -0200

    Animations.

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var image: UIImageView!
+    
+    var timer = Timer()
+    var counter = 1
+    var isAnimating = false
+    
+    @IBAction func next(_ sender: Any) {
+        
+        if isAnimating {
+            timer.invalidate()
+            button.setTitle("Start Animation", for: [])
+            isAnimating = false
+        } else {
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.animate),userInfo: nil, repeats: true)
+            button.setTitle("Stop Animation", for: [])
+            isAnimating = true
+        }
+        
+    }
+    
+    func animate () {
+        
+        image.image = UIImage(named: "frame_\(counter)_delay-0.1s.gif")
+        counter += 1
+        
+        if counter == 37 {
+            counter = 0
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
